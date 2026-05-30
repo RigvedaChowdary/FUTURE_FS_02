375c6a874d582216bd637614d408c853b1aacad5
RIGVEDA CHOWDARY <rigvedachowdary@RIGVEDAs-MacBook-Air.local>
2026-05-30 18:48:58 +0530

    Allow all CORS origins

--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-
 // Middleware
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://future-fs-02-kappa-beige.vercel.app",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 // Routes
